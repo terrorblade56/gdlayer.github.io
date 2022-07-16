e7c76841b9809527ebe136930dd415ec44f8942e
terrorblade56 <109042083+terrorblade56@users.noreply.github.com>
2022-07-16 01:34:07 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * ====================================================================================
+ *                           Google Drive Proxy Player (c) CodySeller
+ * ----------------------------------------------------------------------------------
+ * @copyright This software is exclusively sold at codyseller.com. If you have downloaded this
+ *  from another site or received it from someone else than me, then you are engaged
+ *  in an illegal activity. You must delete this software immediately or buy a proper
+ *  license from https://codyseller.com/license.
+ *
+ *  Thank you for your cooperation and don't hesitate to contact me if anything :)
+ * ====================================================================================
+ *
+ * @author CodySeller (http://codyseller.com)
+ * @link http://codyseller.com
+ * @license http://codyseller.com/license
+ */
+
+
+
+if(!file_exists("includes/config.php")){
+  header("Location: install");
+  exit;
+}
+
+require_once(__DIR__.'/includes/config.php');
+
+$app->run();
